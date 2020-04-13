5fed2fba51aed988df45cee7181e980e0d93d78a
Calvin <cdy3870@gmail.com>
2020-04-13 13:43:24 -0400

    glove

--- ConvNetModel/glove_setup.py
+++ ConvNetModel/glove_setup.py
@@ -12,3 +12,23 @@ from torchvision import datasets, models, transforms
 from torchvision.datasets import CocoDetection
 from tqdm import tqdms
 from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
+
+words = []
+idx = 0
+word2idx = {}
+vectors = bcolz.carray(np.zeros(1), rootdir=f'{glove_path}/6B.50.dat', mode='w')
+
+with open(f'{glove_path}/glove.6B.50d.txt', 'rb') as f:
+    for l in f:
+        line = l.decode().split()
+        word = line[0]
+        words.append(word)
+        word2idx[word] = idx
+        idx += 1
+        vect = np.array(line[1:]).astype(np.float)
+        vectors.append(vect)
+    
+vectors = bcolz.carray(vectors[1:].reshape((400000, 50)), rootdir=f'{glove_path}/6B.50.dat', mode='w')
+vectors.flush()
+pickle.dump(words, open(f'{glove_path}/6B.50_words.pkl', 'wb'))
+pickle.dump(word2idx, open(f'{glove_path}/6B.50_idx.pkl', 'wb'))
